37fd3f90fda0ac9d37d3532476b37e0a167cce95
Realthetical <tooproforyahoo@gmail.com>
2021-09-15 17:13:44 -0500

    Add project files.

---
+++ TNT Email Grabber/TNTNumbers.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TNT_Email_Grabber
+{
+    partial class TNTGrabber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1) Highlight all the emails you want to export\r\n2) Go to File > Save as\r\n3) Drag " +
+    "that file into the application\r\n";
+            // 
+            // TNTGrabber
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 101);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(430, 140);
+            this.MinimumSize = new System.Drawing.Size(430, 140);
+            this.Name = "TNTGrabber";
+            this.Text = "TNT Numbers";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TNTGrabber_FormClosing);
+            this.Load += new System.EventHandler(this.TNTGrabber_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TNT Email Grabber/TNTNumbers.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace TNT_Email_Grabber
+{
+    public partial class TNTGrabber : Form
+    {
+        public TNTGrabber()
+        {
+            InitializeComponent();
+        }
+
+        TNT_Emails.Properties.Settings settings = new TNT_Emails.Properties.Settings();
+        string version = "v0.2";
+        string searchFor = "Job Id        : ";
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string[] readText = File.ReadAllLines(files[0]);
+                
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (string s in readText)
+                {
+                    if (!s.Contains(searchFor))
+                        continue;
+                    sb.AppendLine(s.Replace(searchFor, ""));
+                }
+
+                var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var path = Path.Combine(appDataPath, @"TNTOutput\");
+                string fileName = "output.txt";
+
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                try
+                {
+                    StreamWriter writer = new StreamWriter(path + fileName);
+
+                    writer.Write(sb);
+                    writer.Dispose();
+                    writer.Close();
+                    if(settings.notepadLocation == "")
+                    {
+                        OpenWithDefaultProgram(path + fileName);
+                    }
+                    else
+                    {
+                        Process.Start(settings.notepadLocation, path + fileName);
+                    }
+                    
+                }
+                catch
+                {
+                    MessageBox.Show(string.Format("Error Creating {0}, the file is in use.", fileName), "Error Creating File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+
+            }
+        }
+
+        public static void OpenWithDefaultProgram(string path)
+        {
+            Process fileopener = new Process();
+
+            fileopener.StartInfo.FileName = path;
+            fileopener.Start();
+        }
+
+        private void TNTGrabber_Load(object sender, EventArgs e)
+        {
+            settings.firstOpen = true;
+            this.Text = "TNT Numbers " + version;
+            
+
+            if (settings.firstOpen)
+            {
+                DialogResult dresult = MessageBox.Show("Would you like to set an alternate program to open text files?", "Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dresult == DialogResult.Yes)
+                {
+                    var filePath = string.Empty;
+
+                    using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                    {
+                        openFileDialog.InitialDirectory = "c:\\";
+
+                        if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            filePath = openFileDialog.FileName;
+                        }
+                    }
+
+                    if(!(filePath == ""))
+                    {
+                        //MessageBox.Show(filePath, "File Content at path: " + filePath, MessageBoxButtons.OK);
+                        settings.notepadLocation = filePath;
+                    }
+                    else
+                    {
+                        MessageBox.Show("You selected nothing, default application will be used.", "No Application Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    
+                }
+                else
+                {
+
+                }
+
+                settings.firstOpen = false;
+                
+            }
+        }
+
+        private void TNTGrabber_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            settings.Save();
+        }
+    }
+}
